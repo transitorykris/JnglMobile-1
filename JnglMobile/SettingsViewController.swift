fc055bb01b24e33c00bfd15f3c084ec3dfee5f07
Kris Foster <kris.foster@gmail.com>
2017-07-06 17:31:32 -0700

    Remove didReceiveMemoryWarning from classes since we're not using it yet

--- JnglMobile/SettingsViewController.swift
+++ JnglMobile/SettingsViewController.swift
@@ -23,12 +23,6 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func createConfig() {
